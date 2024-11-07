271090ec37ff506571bf927f7ef208acd979682a
Aleksander Pres <aleksander.pres98@gmail.com>
2024-11-07 02:23:35 +0100

    better connect screen

--- music_downloader/utils.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def get_playlist(url, token):
-    header = {"Authorization": "Bearer " + token}
-    response = requests.get(url, headers=header)
-    data = response.json()
-    return data
