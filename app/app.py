6d4d8147887872a0ed02983881f128187f1d0518
Aleksander Pres <aleksander.pres98@gmail.com>
2024-12-05 23:15:32 +0100

    bug fixed

--- app/app.py
+++ app/app.py
@@ -71,7 +71,15 @@ def home():
     
     playlists = sp.current_user_playlists()['items']
 
-    return render_template('home.html', playlists=playlists)
+    images = []
+    for playlist in playlists:
+        try:
+            image = playlist['images'][0]['url']
+            images.append(image)
+        except TypeError:
+            pass
+
+    return render_template('home.html', data=zip(playlists, images))
 
 
 @app.route('/playlist/<playlist_id>')
